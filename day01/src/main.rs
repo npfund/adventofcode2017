5a8be62f3f674ab11e8ab551c5215e3f7a27e195
Nick Pfundstein <nickpfundstein@gmail.com>
2017-12-02 15:45:02 -0500

    Day 1 Part 1 complete

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -1,3 +1,34 @@
+use std::fs::File;
+use std::io::Read;
+
 fn main() {
-    println!("Hello, world!");
+    part1();
+}
+
+fn part1() {
+    let zero: u8 = 48;
+    let input = File::open("input.txt").expect("File not found!");
+
+    let original_numbers = input.bytes()
+        .map(|x| x.unwrap())
+        .filter(|x| *x > zero)
+        .map(|x| (x - zero) as u32)
+        .peekable()
+        .collect::<Vec<u32>>();
+
+    let mut numbers = Vec::new();
+    let mut num_iter = original_numbers.clone().into_iter().peekable();
+    while let Some(number) = num_iter.next() {
+        if let Some(next) = num_iter.peek() {
+            if number == *next {
+                numbers.push(number);
+            }
+        }
+    }
+
+    if original_numbers.first().unwrap() == original_numbers.last().unwrap() {
+        numbers.push(*original_numbers.first().unwrap())
+    }
+
+    println!("{}", numbers.into_iter().sum::<u32>());
 }

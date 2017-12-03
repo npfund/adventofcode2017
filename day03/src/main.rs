339291ef3ae72cb42d77f1df5bf1677815e9c548
Nick Pfundstein <nickpfundstein@gmail.com>
2017-12-03 17:10:13 -0500

    Day 3 Part 2 complete

--- day03/src/main.rs
+++ day03/src/main.rs
@@ -2,6 +2,7 @@ fn main() {
     let input: f32 = 277678.0;
 
     println!("So {} is {} steps away from the center", input, part1(input));
+    println!("{} is the first value to exceed the input", part2(input as u64));
 }
 
 fn part1(input: f32) -> f32 {
@@ -26,6 +27,70 @@ fn part1(input: f32) -> f32 {
     println!("{} is {} steps away from corner", input, distance_from_corner);
 
     return corner_distance - distance_from_corner;
+}
+
+fn part2(input: u64) -> u64 {
+    let mut matrix = [[0 as u64; 1001]; 1001];
+
+    let mut output = 0;
+    let mut ring = 1;
+
+    let mut x = 500;
+    let mut y = 500;
+
+    matrix[y][x] = 1;
+    x += 1;
+
+    'outer: while input > output {
+        for _ in 0..((2 * ring) - 1) {
+            matrix[y][x] = matrix[y - 1][x] + matrix[y + 1][x] + matrix[y][x + 1] + matrix[y][x - 1]
+                + matrix[y - 1][x - 1] + matrix[y + 1][x + 1] + matrix[y - 1][x + 1] + matrix[y + 1][x - 1];
+            output = matrix[y][x];
+            if output > input {
+                break 'outer;
+            }
+            y -= 1;
+        }
+
+        for _ in 0..(2 * ring) {
+            matrix[y][x] = matrix[y - 1][x] + matrix[y + 1][x] + matrix[y][x + 1] + matrix[y][x - 1]
+                + matrix[y - 1][x - 1] + matrix[y + 1][x + 1] + matrix[y - 1][x + 1] + matrix[y + 1][x - 1];
+            output = matrix[y][x];
+            if output > input {
+                break 'outer;
+            }
+            x -= 1;
+        }
+
+        for _ in 0..(2 * ring) {
+            matrix[y][x] = matrix[y - 1][x] + matrix[y + 1][x] + matrix[y][x + 1] + matrix[y][x - 1]
+                + matrix[y - 1][x - 1] + matrix[y + 1][x + 1] + matrix[y - 1][x + 1] + matrix[y + 1][x - 1];
+            output = matrix[y][x];
+            if output > input {
+                break 'outer;
+            }
+            y += 1;
+        }
+
+        for _ in 0..(2 * ring) {
+            matrix[y][x] = matrix[y - 1][x] + matrix[y + 1][x] + matrix[y][x + 1] + matrix[y][x - 1]
+                + matrix[y - 1][x - 1] + matrix[y + 1][x + 1] + matrix[y - 1][x + 1] + matrix[y + 1][x - 1];
+            output = matrix[y][x];
+            if output > input {
+                break 'outer;
+            }
+            x += 1;
+        }
+
+        matrix[y][x] = matrix[y - 1][x] + matrix[y + 1][x] + matrix[y][x + 1] + matrix[y][x - 1]
+            + matrix[y - 1][x - 1] + matrix[y + 1][x + 1] + matrix[y - 1][x + 1] + matrix[y + 1][x - 1];
+        output = matrix[y][x];
+        x += 1;
+
+        ring += 1;
+    }
+
+    return output;
 }
 
 #[cfg(test)]

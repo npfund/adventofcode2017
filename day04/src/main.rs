08f38be7b49e97f345d1946263fd9d1d02a55571
Nick Pfundstein <nickpfundstein@gmail.com>
2017-12-04 22:45:29 -0500

    Day 4 Part 2 complete

--- day04/src/main.rs
+++ day04/src/main.rs
@@ -2,13 +2,20 @@ use std::fs::File;
 use std::io::BufReader;
 use std::io::BufRead;
 use std::collections::HashMap;
+use std::iter::FromIterator;
 
 fn main() {
     let input = BufReader::new(File::open("input.txt").expect("File not found!"));
     let lines = input.lines()
         .map(|x| x.unwrap()).collect::<Vec<String>>();
 
-    let mut counter = 0;
+
+    println!("{} of {} are valid passphrases", part1(&lines), lines.len());
+    println!("{} of {} are valid passphrases", part2(&lines), lines.len());
+}
+
+fn part1(lines: &Vec<String>) -> u32 {
+    let mut counter: u32 = 0;
     for line in lines.iter() {
         let mut map = HashMap::new();
 
@@ -22,5 +29,26 @@ fn main() {
         }
     }
 
-    println!("{} of {} are valid passphrases", counter, lines.len());
+    return counter;
+}
+
+fn part2(lines: &Vec<String>) -> u32 {
+    let mut counter: u32 = 0;
+    for line in lines.iter() {
+        let mut map = HashMap::new();
+
+        for word in line.split_whitespace() {
+            let mut chars = word.chars().collect::<Vec<char>>();
+            chars.sort_unstable();
+            let sorted = String::from_iter(chars.iter());
+            let entry = map.entry(sorted).or_insert(0);
+            *entry += 1;
+        }
+
+        if map.keys().count() == line.split_whitespace().count() {
+            counter += 1;
+        }
+    }
+
+    return counter;
 }

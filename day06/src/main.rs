41c358b89b783112be758ba27005d66541308c96
Nick Pfundstein <nickpfundstein@gmail.com>
2017-12-09 19:51:49 -0500

    Day 6 Part 2 complete

--- day06/src/main.rs
+++ day06/src/main.rs
@@ -1,6 +1,7 @@
 use std::fs::File;
 use std::io::Read;
 use std::collections::HashSet;
+use std::collections::HashMap;
 
 fn main() {
     let mut input = File::open("input.txt").expect("File not found!");
@@ -12,6 +13,7 @@ fn main() {
         .collect::<Vec<u32>>();
 
     println!("It takes {} cycles to detect a loop", part1(&banks));
+    println!("The loop is {} cycles long", part2(&banks));
 }
 
 fn part1(banks: &Vec<u32>) -> u32
@@ -42,6 +44,34 @@ fn part1(banks: &Vec<u32>) -> u32
     return cycle_count;
 }
 
+fn part2(banks: &Vec<u32>) -> u32
+{
+    let len = banks.len();
+    let mut states = HashMap::new();
+
+    let mut current_state = banks.clone();
+    let mut cycle_count: u32 = 0;
+    while !states.contains_key(&current_state) {
+        states.insert(current_state.clone(), cycle_count);
+
+        let current_state_clone = current_state.clone();
+        let max = current_state_clone.iter()
+            .enumerate()
+            .rev()
+            .max_by(|l, r| l.1.cmp(r.1))
+            .unwrap();
+
+        current_state[max.0] = 0;
+        for i in 1..(max.1 + 1) as usize {
+            current_state[(max.0 + i) % len] += 1;
+        }
+
+        cycle_count += 1;
+    }
+
+    return cycle_count - states.get(&current_state).unwrap();
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
@@ -51,5 +81,12 @@ mod test {
     {
         let banks: Vec<u32> = vec!(0, 2, 7, 0);
         assert_eq!(5, part1(&banks));
+    }
+
+    #[test]
+    fn part2_test()
+    {
+        let banks: Vec<u32> = vec!(0, 2, 7, 0);
+        assert_eq!(4, part2(&banks));
     }
 }
